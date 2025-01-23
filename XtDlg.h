3f6ed578f77db8e6a6aad3f3c015309f0c2652e7
周子皓 <zzh19526308283@outlook.com>
2025-01-23 22:08:15 +0800

    1

--- XtDlg.h
+++ XtDlg.h
@@ -4,6 +4,7 @@
 #include <afxwin.h>
 #include <minwindef.h>
 #include <windef.h>
+#include <afxbutton.h>
 
 
 // XtDlg 对话框
@@ -31,6 +32,26 @@ protected:
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
 public:
+	CMFCButton m_button;
+	CMFCButton m_button1;
+	CMFCButton m_button2;
+	CMFCButton m_button3;
+	CMFCButton m_button4;
+	CMFCButton m_button5;
+	CMFCButton m_button6;
+	CMFCButton m_button7;
+	CMFCButton m_button8;
+	CMFCButton m_button9;
+	CMFCButton m_button10;
+	CMFCButton m_button11;
+	CMFCButton m_button12;
+	CMFCButton m_button13;
+	CMFCButton m_button14;
+	CMFCButton m_button15;
+	CMFCButton m_button16;
+	CMFCButton m_button17;
+	CMFCButton m_button18;
+	CMFCButton m_button19;
 	afx_msg void OnBnClickedBotton();
 	afx_msg void OnBnClickedBotton2();
 	afx_msg void OnBnClickedBotton3();
